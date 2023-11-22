816a6ee9f65a8bf29961d6350b203e8f8e045c1b
Mdshihab706 <sifushihab2002@gmail.com>
2023-11-22 14:29:14 +0600

    slider

--- js/script.js
+++ js/script.js
@@ -37,37 +37,6 @@ back_tp.addEventListener('click',()=>{
   })
 })
 
-
-
-
-
-$('#banner') .slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: true,
-    nextArrow: true,
-  });
-  
-$('.saller_slider') .slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    prevArrow: '<i class="fa-solid fa-chevron-left prevArrow"></i>',
-    nextArrow: '<i class="fa-solid fa-chevron-right nextArrow"></i>',
-  });
-
-$('.hot_slider') .slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    prevArrow: '<i class="fa-solid fa-chevron-left prevArrow"></i>',
-    nextArrow: '<i class="fa-solid fa-chevron-right nextArrow"></i>',
-  });
-
   $(document).ready(function () {
     var elias = document.querySelectorAll("#NZoomImg")
     var arr = Array.from(elias)
---
+++ js/slick_slider.js
@@ -0,0 +1,26 @@
+$('#banner') .slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: true,
+    nextArrow: true,
+  });
+  
+$('.saller_slider') .slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    prevArrow: '<i class="fa-solid fa-chevron-left prevArrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right nextArrow"></i>',
+  });
+
+$('.hot_slider') .slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    prevArrow: '<i class="fa-solid fa-chevron-left prevArrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right nextArrow"></i>',
+  });
